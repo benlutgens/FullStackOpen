ef0cbe78eafb3816aafe00838d7b6069d57618e2
Ben Lutgens <bjlutgens@gmail.com>
2021-02-11 08:48:15 -0600

    3.9 complete. prep for heroku

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 const { json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
 morgan.token('postData', (req, res) => {
